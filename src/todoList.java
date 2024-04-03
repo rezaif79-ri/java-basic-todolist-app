c591ba175554464b137bda448403cd9aeb2f6eb2
Reza IF <rezaif79.ri@gmail.com>
2024-04-03 16:22:08 +0700

    feat: add insert todolist

--- src/todoList.java
+++ src/todoList.java
@@ -1,8 +1,13 @@
 public class todoList {
 
-    public static String[] model = new String[10];
+    public static String[] model = new String[0];
 
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    public static void clearConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     static String readInput(){
         String data = scanner.nextLine();
@@ -27,12 +32,20 @@ public class todoList {
         System.out.println("input: " + input + "\n");
 
         boolean exit = switch (input) {
-            case "1" -> {viewTodoList(); yield false;}
-            case "x", "X" -> { yield true;}
+            case "1" -> {
+                viewTodoList(); 
+                yield false;
+            }
+            case "2" -> {
+                insertTodoList(); 
+                yield false;
+            }
+            case "x", "X" -> { 
+                yield true;
+            }
             default -> {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-                System.out.println("Input salah \n");
+                clearConsole();
+                System.out.println("Invalid input! \n");
                 yield false;
             }
         };
@@ -41,6 +54,32 @@ public class todoList {
     }
 
     static void viewTodoList(){
-        System.out.println("view todolist");
+        clearConsole();
+        System.out.println("=== ToDo List ===");
+
+        if (model.length == 0){
+            clearConsole();
+            System.out.println("List kosong \n");
+        }else {
+            for(var i = 0; i < model.length; i++) {
+                System.out.println((i+1) + ". " + model[i]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void insertTodoList(){
+        clearConsole();
+        System.out.println("===== TodoList =====");
+        System.out.print("Todo: ");
+        var input = readInput();
+
+        var temp = model;
+        model = new String[model.length + 1];
+        for(var i = 0; i < temp.length; i++) {
+            model[i] = temp[i];
+        }
+        model[model.length-1] = input;
+        System.out.println();
     }
 }
